1b9d7defd3f9781fcd23140b8f78c8750cf07b52
星本泰憲 <74086136+hoshimotoyasunori@users.noreply.github.com>
2022-01-06 00:58:31 +0900

    20220105last

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import Layout from './components/layout';
 
 const MyApp =({ Component, pageProps }) => {
-  // const getLayout = Component.getLayout || ((page) =>  page);
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import index from '../styles/Index.module.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className={index.container}>
       <Image src="/tehon.png" width={400} height={400}  alt="tehon main" />
     </div>
 
